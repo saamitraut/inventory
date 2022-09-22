b11f40d37dd6916af5574a6401af55a59e84a0a9
saamitraut <saamitraut@gmail.com>
2022-09-22 17:16:18 +0530

    xxx

--- resources/views/branch_master/index.blade.php
+++ resources/views/branch_master/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="container-xxl flex-grow-1 container-p-y">
   
-  <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Masters /</span> Manage Branch Master</h4>
+  <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Masters/</span> Manage Branch Master</h4>
   
 
 @if(Session::has('message'))
