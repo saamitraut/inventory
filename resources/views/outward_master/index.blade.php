71d5708be021c4334a3e8a31a7c1eca657564966
saamitraut <saamitraut@gmail.com>
2022-12-12 17:34:38 +0530

    xxx

--- resources/views/outward_master/index.blade.php
+++ resources/views/outward_master/index.blade.php
@@ -1,41 +1,88 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Laravel Crud By PHP Code Builder</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-</head>
-<body>
+@include('includes.header')
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="http://crudegenerator.in">Laravel Crud By PHP Code Builder</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active" ><a href="{{Request::root()}}/outward_master">Manage Outward_master</a></li>
-        <li><a href="{{Request::root()}}/outward_master/add-outward_master">Add Outward_master</a></li>
-      </ul>
-  </div>
-</nav>
+<!-- Content wrapper -->
+<div class="content-wrapper">
+  <!-- Content starts -->
+  
+  <div class="container-xxl flex-grow-1 container-p-y">
+    
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Manage Outward_master</h4>
 
-<div class="container">
-
-  <h2>Manage Outward_master</h2>
-
+  
 @if(Session::has('message'))
   <div class="alert alert-success">
                     <strong><span class="glyphicon glyphicon-ok"></span>{{  Session::get('message') }}</strong>
                 </div>
 @endif
 
+<!-- Button trigger modal -->
+<button
+type="button"
+class="btn btn-primary"
+data-bs-toggle="modal"
+data-bs-target="#basicModal" style="margin-bottom: 15px"
+>
+Add Outward Entries
+</button>
+<!-- Modal -->
+<div class="modal fade" id="basicModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel1">Add Outward Entries</h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+          <form role="form" method="post" action="/outward_master/add-outward_master-post" >
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <div class="form-group">
+    <label for="material_id">Material_id:</label>
+    <input type="number" class="form-control" id="material_id" name="material_id">
+  </div>
+    <div class="form-group">
+    <label for="material_description">Material_description:</label>
+    <input type="text" class="form-control" id="material_description" name="material_description">
+  </div>
+    <div class="form-group">
+    <label for="opening_stock">Opening_stock:</label>
+    <input type="number" class="form-control" id="opening_stock" name="opening_stock">
+  </div>
+    <div class="form-group">
+    <label for="issued">Issued:</label>
+    <input type="number" class="form-control" id="issued" name="issued">
+  </div>
+    <div class="form-group">
+    <label for="closing_stock">Closing_stock:</label>
+    <input type="number" class="form-control" id="closing_stock" name="closing_stock">
+  </div>
+    <div class="form-group">
+    <label for="unit_id">Unit_id:</label>
+    <input type="number" class="form-control" id="unit_id" name="unit_id">
+  </div>
+              
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+          Close
+        </button>
+        <button type="submit" class="btn btn-primary">Save changes</button> </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal end -->
+
 @if(count($outward_masters)>0)
   <table class="table table-hover">
     <thead>
       <tr>
         <th>SL No</th>
-        <th>material_id</th>
+        <th>Material</th>
        <th>Actions</th>
       </tr>
     </thead>
@@ -44,8 +91,8 @@
 @foreach($outward_masters as $outward_master)
       <tr>
         <td>{{$i}} </td>
-        <td> <a href="{{Request::root()}}/outward_master/view-outward_master/{{$outward_master->id}}" > {{$outward_master->material_id }}</a> </td>
-
+        {{-- <td> <a href="{{Request::root()}}/outward_master/view-outward_master/{{$outward_master->id}}" > {{$outward_master->material_id }}</a> </td> --}}
+<td> <a href="{{Request::root()}}/inward_master/view-inward_master/{{$outward_master->id}}" > {{$materials[$outward_master->material_id]['name'] }}</a> </td>  
         <td>
         <a href="{{Request::root()}}/outward_master/change-status-outward_master/{{$outward_master->id }}" > @if($outward_master->status==0) {{"Activate"}}  @else {{"Dectivate"}} @endif </a>
         <a href="{{Request::root()}}/outward_master/edit-outward_master/{{$outward_master->id}}" >Edit</a>
@@ -64,5 +111,5 @@
  @endif
 </div>
 
-</body>
-</html>
+</div>
+@include('includes.footer')
